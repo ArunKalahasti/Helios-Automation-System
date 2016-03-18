19bdbd33be3f75466354dbb369a5ebcde7492681
johngreen-uta <johnjgreen@mavs.uta.edu>
2016-03-17 19:47:15 -0500

    current moniter and mqtt publish

--- CurrentMonitor/Arduino/current_monitor/current_monitor.ino
+++ CurrentMonitor/Arduino/current_monitor/current_monitor.ino
@@ -5,15 +5,15 @@ EnergyMonitor cm1;
 void setup()
 {    
   Serial.begin(9600); 
-  cm1.current(1, 120); // Current: input pin, calibration.
+  cm1.current(1, 55); // Current: input pin, calibration.
 }
 
 void loop()
 {
-  double Irms = cm1.calcIrms(1450)-.55; // Clac Irms: num of reads - an offset
+  double Irms = cm1.calcIrms(1450); // Clac Irms: num of reads - an offset
 
   if(Irms<0.04){ //readings under 0.04 is assumed noise
     Irms = 0;
   }
-  Serial.println(Irms*115.0); // current x voltage
+  Serial.println(Irms*120.0); // current x voltage
 }
